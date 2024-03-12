58b6aca2b82e137517b5bdd25d9a52ecddd9870e
tandra048 <2048tandraju@gmail.com>
2024-03-12 22:20:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/blogs/Blogs'
+function App() {
+
+
+  return (
+    <>
+     
+     <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
